8bef3843ed6423b9176695863ca6847da3e0343e
mathkruger <ms-kruger@hotmail.com>
2022-09-22 14:25:38 -0300

    changing the function binding

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-let audio = null;
+window.onload = function () {
+    let audio = null;
 
-const executeSound = (soundName) => {
-    if (audio === null) {
-        audio = new Audio(`${soundName}.mp3`);
-        audio.play();
-        audio.addEventListener("ended", () => {
-            audio = null;
-        });
+    const executeSound = (soundName) => {
+        if (audio === null) {
+            audio = new Audio(`sounds/${soundName}.mp3`);
+            audio.play();
+            audio.addEventListener("ended", () => {
+                audio = null;
+            });
+        }
     }
-}
 
-const webSocketMethods = {
-    onOpen = () => {
-        console.log("[Marli Soundboard] connection opened");
-    },
-    onClose = () => {
-        console.log("[Marli Soundboard] connection closed");
-    },
-    onMessage = (message) => {
-        const json = JSON.parse(message.data);      
-        executeSound(json.audioId);  
-    },
-    onError = () => {
-        console.log("[Marli Soundboard] websocket error!");
+    const webSocketMethods = {
+        onOpen = () => {
+            console.log("[Marli Soundboard] connection opened");
+        },
+        onClose = () => {
+            console.log("[Marli Soundboard] connection closed");
+        },
+        onMessage = (message) => {
+            const json = JSON.parse(message.data);
+            executeSound(json.audioId);
+        },
+        onError = () => {
+            console.log("[Marli Soundboard] websocket error!");
+        }
+    };
+
+    const connectToWebSocket = (wsUrl) => {
+        const webSocket = new WebSocket(wsUrl);
+        webSocket.onopen = webSocketMethods.onOpen;
+        webSocket.onclose = webSocketMethods.onClose;
+        webSocket.onmessage = webSocketMethods.onMessage;
+        webSocket.onerror = webSocketMethods.onError;
+    };
+
+    window.initMarliSoundboard = function (wsUrl) {
+        connectToWebSocket(wsUrl);
     }
-};
-
-const connectToWebSocket = (wsUrl) => {
-    const webSocket = new WebSocket(wsUrl);
-    webSocket.onopen = webSocketMethods.onOpen;
-    webSocket.onclose = webSocketMethods.onClose;
-    webSocket.onmessage = webSocketMethods.onMessage;
-    webSocket.onerror = webSocketMethods.onError;
-};
-
-window.initMarliSoundboard = function(wsUrl) {
-    connectToWebSocket(wsUrl);
 }
