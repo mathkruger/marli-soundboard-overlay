aec0ff8db297465897e8e7f23a370b3f150517db
mathkruger <ms-kruger@hotmail.com>
2022-09-22 13:26:34 -0300

    first

---
+++ index.js
@@ -0,0 +1,39 @@
+let audio = null;
+
+const executeSound = (soundName) => {
+    if (audio === null) {
+        audio = new Audio(`${soundName}.mp3`);
+        audio.play();
+        audio.addEventListener("ended", () => {
+            audio = null;
+        });
+    }
+}
+
+const webSocketMethods = {
+    onOpen = () => {
+        console.log("[Marli Soundboard] connection opened");
+    },
+    onClose = () => {
+        console.log("[Marli Soundboard] connection closed");
+    },
+    onMessage = (message) => {
+        const json = JSON.parse(message.data);      
+        executeSound(json.audioId);  
+    },
+    onError = () => {
+        console.log("[Marli Soundboard] websocket error!");
+    }
+};
+
+const connectToWebSocket = (wsUrl) => {
+    const webSocket = new WebSocket(wsUrl);
+    webSocket.onopen = webSocketMethods.onOpen;
+    webSocket.onclose = webSocketMethods.onClose;
+    webSocket.onmessage = webSocketMethods.onMessage;
+    webSocket.onerror = webSocketMethods.onError;
+};
+
+Window.initMarliSoundboard = (wsUrl) => {
+    connectToWebSocket(wsUrl);
+}
