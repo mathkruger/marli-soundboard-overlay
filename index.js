59594495342f83e5180a617d7afd0fb376b421ea
mathkruger <ms-kruger@hotmail.com>
2022-09-27 14:56:19 -0300

    rewriting overlay

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
-window.onload = function () {
-    let audio = null;
+const listenToWebSocket = (fn) => {
+    let property = Object.getOwnPropertyDescriptor(MessageEvent.prototype, "data");
+    const data = property.get;
 
-    const executeSound = (soundName) => {
-        if (audio === null) {
-            audio = new Audio(`sounds/${soundName}.mp3`);
-            audio.play();
-            audio.addEventListener("ended", () => {
-                audio = null;
-            });
+    function lookAtMessage() {
+        let socket = this.currentTarget instanceof WebSocket;
+
+        if (!socket) {
+            return data.call(this);
         }
+
+        let msg = data.call(this);
+
+        Object.defineProperty(this, "data", { value: msg });
+        fn({ data: msg, socket: this.currentTarget, event: this });
+        return msg;
     }
 
-    const webSocketMethods = {
-        onOpen: (webSocket, sso) => {
-            webSocket.send(JSON.stringify({ header: "sso", data: { ticket: sso } }));
-            console.log("[Marli Soundboard] connection opened");
-        },
-        onClose: () => {
-            console.log("[Marli Soundboard] connection closed");
-        },
-        onMessage: (message) => {
-            const json = JSON.parse(message.data);
-            executeSound(json.audioId);
-        },
-        onError: (e) => {
-            console.log("[Marli Soundboard] websocket error!", e);
+    property.get = lookAtMessage;
+    Object.defineProperty(MessageEvent.prototype, "data", property);
+};
+
+const parseMessage = (message) => {
+    if (message.indexOf("habblet/open/") > -1) {
+        const jsonText = message.split("habblet/open/")[1];
+        
+        if (jsonText) {
+            const json = JSON.parse(jsonText);
+            if (json.header === "marli_audio_play") {
+                executeSound(json.data.audioId);
+            }
         }
-    };
-
-    const connectToWebSocket = (wsUrl, sso) => {
-        const webSocket = new WebSocket(wsUrl);
-        webSocket.onopen = () => { webSocketMethods.onOpen(webSocket, sso) };
-        webSocket.onclose = webSocketMethods.onClose;
-        webSocket.onmessage = webSocketMethods.onMessage;
-        webSocket.onerror = webSocketMethods.onError;
-    };
-
-    window.initMarliSoundboard = function (wsUrl, sso) {
-        connectToWebSocket(wsUrl, sso);
     }
-}
+};
+
+const executeSound = (soundName) => {
+    let audio = document.querySelector("#app");
+
+    if (audio.children.length === 0) {
+        audio.innerHTML += `<audio autoplay><source src="sounds/${soundName}.mp3"></audio>`;
+        
+        audio.querySelector("audio").addEventListener("ended", () => {
+            audio.innerHTML = '';
+        });
+    }
+};
+
+window.initMarliSoundboard = function () {
+    listenToWebSocket(({ data }) => {
+        data.text().then(text => {
+            parseMessage(text);
+        });
+    });
+};
